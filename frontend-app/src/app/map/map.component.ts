2ffdb34af0ec5e85f3cc368c351113c6bfef0af3
cmeciualex <alexandru.cmeciu@stud.ubbcluj.ro>
2026-03-04 20:21:05 +0200

    frontendok

--- frontend-app/src/app/map/map.component.ts
+++ frontend-app/src/app/map/map.component.ts
@@ -1,42 +1,60 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+// Importăm funcția cu datele hardcodate din fișierul nou creat
+import { getMarkers } from './markers';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [],
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css'] // Am folosit .css în loc de .scss
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements AfterViewInit {
 
   private map: L.Map | undefined;
 
-  // Coordonatele centrale și nivelul de zoom din exemplu (Aeroport Oradea)
-  private centroid: L.LatLngExpression = [47.026739, 21.901495];
-
-  private initMap(): void {
-    // Inițializează harta pe elementul cu ID-ul 'map'
-    this.map = L.map('map', {
-      center: this.centroid,
-      zoom: 15.2
-    });
-
-    // Adaugă stratul de hărți (tile layer) de la OpenStreetMap
-    const tiles = L.tileLayer('https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=MCpv62w29YtAq27NFF3G', {
-      maxZoom: 18,
-      minZoom: 10,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
-
-
-  }
+  // Coordonatele oficiale ale centrului aeroportului Oradea (ARP)
+  private centroid: L.LatLngExpression = [47.023333, 21.901944];
 
   constructor() { }
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.renderHardcodedMarkers();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: this.centroid,
+      zoom: 15.48888888 // Zoom-ul ușor mărit pentru a vedea mai bine pista
+    });
+
+    const tiles = L.tileLayer('https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=MCpv62w29YtAq27NFF3G', {
+      maxZoom: 18,
+      minZoom: 10,
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  private renderHardcodedMarkers(): void {
+    if (!this.map) return;
+
+    // Aplicăm logica din proiectul GitHub: preluăm array-ul și îl parcurgem
+    const markers = getMarkers();
+
+    markers.forEach(marker => {
+      // Adăugăm markerul pe hartă
+      marker.addTo(this.map!);
+
+      // Opțional: Deschidem popup-ul automat pentru primul marker critic
+      if (marker.options.title?.includes('Fisură')) {
+        marker.bindPopup(`<b>Detecție Dronă:</b> ${marker.options.title}`).openPopup();
+        marker.bindPopup(`<img src="drum_test.jpg" alt="marker" style="width:130px;">`).openPopup();      } else {
+        //aici am chestia asta momentan doar asa
+        marker.bindPopup(`<b>Info:</b> ${marker.options.title}`);
+      }
+    });
   }
 }
